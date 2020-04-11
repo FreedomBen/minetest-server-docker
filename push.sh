5d7861a324d33d27b1232800dcfa929d58cb90e6
Benjamin Porter <FreedomBen@users.noreply.github.com>
2020-04-11 10:43:44 -0600

    Attempt to get fully statically linked build

---
+++ push.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo podman push --authfile ~/.docker/config.json quay.io/freedomben/minetest:latest
--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
   #--user root \
-sudo podman run --rm -it \
+#  --publish "30000:30000/udp" \
+sudo podman run -it \
   --publish "30303:30303/udp" \
-  quay.io/freedomben/minetest:latest bash
+  --name "minetest" \
+  quay.io/freedomben/minetest:latest
+  #quay.io/freedomben/minetest:latest bash
 #sudo podman run --rm -it \
 #  --volume "$(pwd)/minetest:/home/docker/.minetest" \
 #  --publish "30303:30303/udp" \
+#  --name "minetest" \
 #  quay.io/freedomben/minetest:latest bash
